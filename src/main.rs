30afb3719f578daba9f53006e57d3d6c32cba9f3
Ismail Boujdad <iboujdad@c1r9p11.1337.ma>
2026-09-09 12:49:30 +0100

    commandlines

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,9 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     let mut cursor_y = 0;
     let mut gcursor = 0;
     let mut mode = 0;
+   let mut the_command_line = String::new();
     loop {
-        terminal.draw(|frame| renderer(frame, &input_box, cursor_x, cursor_y, gcursor, mode))?;
+        terminal.draw(|frame| renderer(frame, &input_box, cursor_x, cursor_y, gcursor, mode, &the_command_line))?;
 
         let event = crossterm::event::read()?;
         let the_text = &input_box.clone();
@@ -27,7 +28,7 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
         if let crossterm::event::Event::Key(event_key) = event {
             match mode {
                 0 => { ////////////////////// NORMAL MODE ////////////////////////
-                       if !modes::normal_mode(terminal, event_key, &mut mode, &mut cursor_x, &mut cursor_y, &mut gcursor, &mut splitted).unwrap() {
+                       if !modes::normal_mode(terminal, event_key, &mut mode, &mut cursor_x, &mut cursor_y, &mut gcursor, &mut splitted, &mut the_command_line).unwrap() {
                            break;
                        }
                 }
@@ -35,7 +36,13 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
                        if !modes::insert_mode(terminal, event_key, &mut mode, &mut cursor_x, &mut cursor_y, &mut gcursor, &mut input_box, &mut splitted).unwrap(){
                            continue;
                        }
-                } /////////////////// END OF INSERT MODE /////////////////////////
+                }
+                2 => { ////////////////////// COMMAND MODE ////////////////////////////////
+                        if !modes::command_mode(event_key, &mut mode, &mut the_command_line).unwrap()
+                        {
+                            break;
+                        }
+                }
                 _ => {}
             }
         }
@@ -45,33 +52,46 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
 
 
 
-fn renderer(frame: &mut Frame, input_box: &str, cursor_x: i32, cursor_y: i32, gcursor: i32, mode: i32){
+fn renderer(frame: &mut Frame, input_box: &str, cursor_x: i32, cursor_y: i32, gcursor: i32, mode: i32, the_command_line: &str){
     let areas = ratatui::layout::Layout::vertical([
         ratatui::layout::Constraint::Min(0),
         ratatui::layout::Constraint::Length(1)
     ]).split(frame.area());
 
     let footer_text: String;
+    let bottom_chunk = ratatui::layout::Layout::horizontal([
+        ratatui::layout::Constraint::Percentage(50),
+        ratatui::layout::Constraint::Percentage(50)
+    ]).split(areas[1]);
 
     match mode {
         0 => {
-            footer_text = format!("NORMAL         {}, {}  BLUR V0.1", cursor_x, cursor_y);
+            footer_text = format!("NORMAL");
         }
         1 => {
-            footer_text = format!("INSERT         {}, {}  BLUR V0.1", cursor_x, gcursor);
+            footer_text = format!("INSERT");
+        }
+        2 => {
+            footer_text = format!("{}", the_command_line);
         }
         _ => {footer_text = "SOME ERRORS, try to relaunch the program                   BLUR V0.1".to_string();}
     }
 
-    let footer = ratatui::widgets::Paragraph::new(footer_text)
+    let footer_mode = ratatui::widgets::Paragraph::new(footer_text)
                 .alignment(ratatui::layout::Alignment::Left)
+                .style(ratatui::style::Style::default()
+                    .fg(Black)
+                    .bg(White));
+    let footer_copyrights = ratatui::widgets::Paragraph::new(format!("{}, {}   BLUR V0.1" , cursor_x, gcursor))
+                .alignment(ratatui::layout::Alignment::Right)
                 .style(ratatui::style::Style::default()
                     .fg(Black)
                     .bg(White));
 
     let input = ratatui::text::Text::from(input_box);
     frame.render_widget(input, areas[0]);
-    frame.render_widget(footer, areas[1]);
+    frame.render_widget(footer_mode, bottom_chunk[0]);
+    frame.render_widget(footer_copyrights, bottom_chunk[1]);
 
     frame.set_cursor_position((
             areas[0].x + cursor_x as u16,
