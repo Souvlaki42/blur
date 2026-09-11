0cfd73066e9f73bbf5c2bca7027f79ece5f55c09
Ismail Boujdad <iboujdad@c1r4p12.1337.ma>
2026-09-11 04:55:57 +0100

    deleting unwanted files...

--- first.py
+++
@@ -1,6 +0,0 @@
-def main():
-  print("My First Pythonic Code in My own textEditor, it has no colors sadly :(")
-
-
-if __name__ == "__main__:
-   main()
--- first_code.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-
-int main() 
-{
-   printf("My First Code in My own Text Editor");
-}
